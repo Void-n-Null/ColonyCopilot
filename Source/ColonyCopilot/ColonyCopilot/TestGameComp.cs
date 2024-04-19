662d8bf9a3a5478d3973288ae14acbaf1fbe0702
Void & Null <kosmix1999@gmail.com>
2024-04-19 11:48:38 -0500

    Finalized

--- Source/ColonyCopilot/ColonyCopilot/TestGameComp.cs
+++ Source/ColonyCopilot/ColonyCopilot/TestGameComp.cs
@@ -1,9 +1,8 @@
 using UnityEngine;
 using System;
-using OpenAI;
-using OpenAI.Chat;
+using OpenAI_API;
+using OpenAI_API.Chat;
 using Verse;
-using Message = OpenAI.Chat.Message;
 
 namespace ColonyCopilot;
 
@@ -18,22 +17,19 @@ public class TestGameComp : GameComponent
     public override void LoadedGame()
     {
         Log.Message($"COLONY COPILOT: LoadedGame() called!");
-        var auth = new OpenAI.OpenAIAuthentication("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");
-        var openAIClient = new OpenAI.OpenAIClient(auth);
-        var messages = new List<Message>
-        {
-            new(Role.System, "You are a snarky AI that says things like 'I'm sorry Dave, I'm afraid I can't do that'"),
-            new(Role.User, "Hello, AI!"),
-        };
-        var request = new ChatRequest(messages);
-        var completion = openAIClient.ChatEndpoint.GetCompletionAsync(request);
+        var auth = new APIAuthentication("sk-proj-U38PNrUNI0bDmuAoWrHOT3BlbkFJ4ffBv3FfcSolwvSPeuKi");
+        var openAIClient = new OpenAIAPI(auth);
+        var chat = openAIClient.Chat.CreateConversation();
+        chat.AppendMessage(new ChatMessage(ChatMessageRole.System, "You are a snarky AI who says snarky things."));
+        chat.AppendMessage(new ChatMessage(ChatMessageRole.User, "Hello!"));
+        var completion = chat.GetResponseFromChatbotAsync();
         completion.ContinueWith(completedTask => OnMessageCompletion(completedTask.Result));
         completion.Start();
     }
     
-    public void OnMessageCompletion(ChatResponse response)
+    public void OnMessageCompletion(string response)
     {
         Log.Message("COLONY COPILOT: OnMessageCompletion() called!");
-        Log.Message(response.FirstChoice.Message.Content);
+        Log.Message(response);
     }
 }
