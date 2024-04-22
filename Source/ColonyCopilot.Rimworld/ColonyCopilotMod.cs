6d77494360157f2449a21908879a0b3fe0fa25a4
Void & Null <70048414+Void-n-Null@users.noreply.github.com>
2024-04-22 17:55:56 -0500

    Sweet

---
+++ Source/ColonyCopilot.Rimworld/ColonyCopilotMod.cs
@@ -0,0 +1,10 @@
+﻿using Verse;
+
+namespace ColonyCopilot.Rimworld;
+public class ColonyCopilotMod : Mod
+{
+    public ColonyCopilotMod(ModContentPack content) : base(content)
+    {
+        
+    }
+}
