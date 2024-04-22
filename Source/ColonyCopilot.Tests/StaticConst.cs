263cf735906bbff0222a3d294cb9ae15320459da
Void & Null <kosmix1999@gmail.com>
2024-04-22 17:55:37 -0500

    Quick Source removal to update gitignore

--- Source/ColonyCopilot.Tests/StaticConst.cs
+++
@@ -1,8 +0,0 @@
-
-namespace ColonyCopilot.Tests
-{
-    public static class StaticConst
-    {
-        public const string APIKey = "sk-proj-9Tk5USs6Lv9NKNKFWimLT3BlbkFJC2P41Pvfbmk1tPXmVWyb";
-    }
-}
